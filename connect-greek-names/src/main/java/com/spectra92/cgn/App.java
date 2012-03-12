1f93f09f97db66629b118aa805b0ab7850453309
Trelo Bifteki <lambrospd@gmail.com>
2012-03-12 19:37:44 +0000

    Adding more

---
+++ connect-greek-names/src/main/java/com/spectra92/cgn/App.java
@@ -0,0 +1,13 @@
+package com.spectra92.cgn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
